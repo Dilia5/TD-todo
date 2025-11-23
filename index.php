b65617b3f82886b0901c74b4ae57d116f0824323
gloryGloriouse <audreyglory31@gmail.com>
2025-11-23 17:00:26 +0100

    update names

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
         <?php endif; ?>
         <form method="post" action="index.php" class="row g-3 pt-5 justify-center d-flex justify-content-center">
             <div class="col-4 ">
-                <input type="text" name="title" class="form-control" id="inputPassword2" placeholder="Task Title">
+                <input type="text" name="tache" class="form-control" id="inputPassword2" placeholder="Task Title">
             </div>
             <div class="col-auto">
                 <button type="submit" name="action" class="btn btn-primary mb-3" value="new">Add</button>
@@ -38,18 +38,18 @@
                         <?php foreach ($taches as $t):?>
                             <?php if($t["done"] == 1):?>
                                 <li class="list-group-item list-group-item-success d-flex justify-content-between"> 
-                                     <span class="me-3 flex-shrink-1 " style=" word-break: break-word; "><?= $t["title"] ?></span>
+                                     <span class="me-3 flex-shrink-1 " style=" word-break: break-word; "><?= $t["tache"] ?></span>
                                     <form method="post" action="index.php" class="btn-group flex-shrink-0 " role="group" aria-label="Basic outlined example">
-                                        <input type="hidden" name="idtodo" value="<?= $t["id"]?>">
+                                        <input type="hidden" name="idtodo" value="<?= $t["idtodo"]?>">
                                         <button type="submit" value="toggle" name="action" class="btn btn-primary rounded-start">Done</button>
                                         <button type="submit" value="delete" name="action" class="btn btn-danger">X</button>
                                     </form>
                                 </li>
                             <?php else:?>
                                 <li class="list-group-item list-group-item-warning d-flex justify-content-between">
-                                     <span class="me-3 flex-shrink-1" style="word-break: break-word;"><?= $t["title"] ?></span>
+                                     <span class="me-3 flex-shrink-1" style="word-break: break-word;"><?= $t["tache"] ?></span>
                                     <form method="post"  action="index.php" class="btn-group flex-shrink-0" role="group" aria-label="Basic outlined example">
-                                        <input type="hidden" name = "idtodo" value="<?= $t["id"]?>">
+                                        <input type="hidden" name = "idtodo" value="<?= $t["idtodo"]?>">
                                         <button type="submit" value="toggle" name="action" class="btn btn-primary rounded-start">Done</button>
                                         <button type="submit" value="delete" name="action" class="btn btn-danger">X</button>
                                     </form>
